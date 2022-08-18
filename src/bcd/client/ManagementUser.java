86d7462f9a3557fb2911f94264c419a3189b7fc3
Jing Yang Chin <jychin47@gmail.com>
2022-08-18 10:04:37 +0800

    Second_version

---
+++ src/bcd/client/ManagementUser.java
@@ -0,0 +1,11 @@
+package bcd.client;
+
+public class ManagementUser extends User{
+    public ManagementUser(String name, int age, String email){
+        super(name, age, email);
+
+    }
+    public void createRecord(){
+
+    }
+}
--- src/bcd/client/User.java
+++ src/bcd/client/User.java
@@ -3,4 +3,13 @@ package bcd.client;
 public class User {
     public User(String name, int age, String email) {
     }
+    public void login(String email, String password){
+
+    }
+    public void logout(){
+        System.out.println("Logout successfully");
+    }
+    public void viewRecord(String record){
+
+    }
 }

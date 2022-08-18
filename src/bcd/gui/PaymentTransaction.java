afafded3485f3551f1efb378f05c8c1ab907f5fb
lawoo <lawoozhe@gmail.com>
2022-08-18 22:51:42 +0800

    Merge remote-tracking branch 'origin/master'

--- src/bcd/gui/PaymentTransaction.java
+++ src/bcd/gui/PaymentTransaction.java
@@ -1,4 +1,11 @@
 package bcd.gui;
 
+import javax.swing.*;
+
 public class PaymentTransaction {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton printReceiptButton;
+    private JButton paymentButton;
 }
